7c7ba951953541e816b6815932aaf5a88fad24c7
rakibuzzamanashik <rakibuzzamanashik@users.noreply.github.com>
2020-05-24 05:20:21 +0600

    2nd Commit [live world data]
    Attached image, live data of the whole world is showing properly. No graph yet.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 import React from 'react';
+import Cards from './components/Cards/Cards';
+import Chart from './components/Chart/Chart';
+import CountryPicker from './components/CountryPicker/CountryPicker';
+import styles from './App.module.css';
+import {fetchData} from './api';
+
 
 class App extends React.Component {
+    state = {
+        data: {},
+    }
+
+
+    async componentDidMount () {
+        const apiData = await fetchData();
+
+        this.setState({data: apiData});
+    }
+
     render() {
+        const {data} = this.state;
+
         return (
-            <div>
-                <h1>Covid-19 Web App</h1>
+            <div className={styles.container}>
+                <Cards data={data} />
+                {/* <Chart/>
+                <CountryPicker></CountryPicker> */}
             </div>
         )
     }
